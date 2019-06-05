c8508db739d5f9311cdce4dd3c56144d0e5912ff
stephandanyluk <stephan.danyluk@progleasing.com>
2019-06-05 16:14:37 -0600

    Added additional comments/description to initial values function

--- R/print_initial_vals.R
+++ R/print_initial_vals.R
@@ -1,15 +1,15 @@
-#' A Cat Function
+#' Print Intitial Vals Function
 #'
-#' This function allows you to express your love of cats.
+#' Given a dataframe and a number x, prints the first x non-na values of each column (though prints first 5 values if no number "x" is specified).
 #' @param df A DataFrame
 #' @param x Number of non-na items to print for each column.
-#' @keywords cats
+#' @keywords dataframe
 #' @export
 #' @examples
-#' cat_function()
+#' print_initial_vals(iris)
 
 # Print the first x non-na values for each column in dataframe "df"
-print_initial_vals <- function(df, x) {
+print_initial_vals <- function(df, x = 5) {
   for (i in 1:ncol(df)) {
     cat("First ", x, " non-NA values of ", names(df)[i], ":", df[which(!is.na(df[,i]))[1:x],i], "\n")
   }
